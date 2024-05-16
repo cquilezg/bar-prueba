ade67959dab1ce3b00fa936fcf9314c888b0a112
GitHub Action <action@github.com>
2024-05-16 16:24:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/carmeloquilez/barprueba/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/carmeloquilez/barprueba/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.carmeloquilez.barprueba.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.carmeloquilez.barprueba.MyBundle
+import com.github.carmeloquilez.barprueba.services.MyProjectService
 import javax.swing.JButton
 
 
